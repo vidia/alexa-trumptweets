84472de6919f67591175fcfa633a58e2650bbbf2
David Tschida <dmtschida1@gmail.com>
2017-06-23 19:50:18 -0400

    Working v1

---
+++ README.md
@@ -0,0 +1,11 @@
+# Alexa Reads Trump Tweets
+
+I made this to allow Alexa to read Donald Trump tweets on @realDonaldTrump. Ask her to "Ask Trump to read a tweet" and she will read off the most recent tweet to you as best she can. 
+
+## Issues
+
+Please let me know which tweets she doesn't read well. Make an issue with the link to the tweet or the day it was tweeted if you can. Thanks. 
+
+## Developing/Running
+
+Just make a secrets.js file containing the proper input for the Twitter client NPM repo and it should start working for you. 
---
+++ index.js
@@ -0,0 +1,90 @@
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
+/**
+* This sample demonstrates a simple skill built with the Amazon Alexa Skills
+* nodejs skill development kit.
+**/
+
+'use strict';
+
+const Alexa = require('alexa-sdk');
+const Twitter = require("twitter");
+var APP_ID = undefined;
+
+function getLastTweet(callback) {
+    var client = new Twitter(require("./secret.js"));
+
+    var params = {
+        screen_name: 'realDonaldTrump',
+        count: 1,
+        trim_user: true,
+        exclude_replies: true,
+    };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (error) callback(error); 
+        var tweet = tweets[0];
+        var prefix = "This tweet has some "; 
+
+        var hasMedia = tweet.entities.media.length > 0; 
+        var hasUrl = tweet.entities.urls.length > 0; 
+
+        if(hasMedia && hasUrl) {
+            prefix = "This tweet has some media and a url in it. ";
+        } else if (hasMedia) {
+            prefix = "There is some media to this tweet. ";
+        } else if(hasUrl) {
+            prefix = "There is a link in this tweet";
+        }
+
+        var finalResponse = ""; 
+        if(hasMedia || hasUrl) {
+            finalResponse = prefix; 
+        }
+
+        var rawTweet = tweet.text; 
+        var processedTweet = rawTweet; 
+        if(hasMedia) {
+            for(var i = 0; i < tweet.entities.media.length; i++) {
+                processedTweet = processedTweet.replace(tweet.entities.media[i].url, '<emphasis level="reduced"> There is a video here titled ' + tweet.extended_entities.media[i].additional_media_info.title + '</emphasis>');
+            }
+        }
+
+        finalResponse = finalResponse + processedTweet; 
+        
+        callback(false, finalResponse);
+    });
+}
+
+// Some debug statements for running it locally to test things. 
+// getLastTweet(function(err, tweet) {
+//     if(err) throw err;
+//     console.log(tweet);
+// });
+
+const handlers = {
+    'GetLastTweetIntent': function () {
+        var handler = this;
+        getLastTweet(function (err, readableTweet) {
+            if(err) return handler.emit(':tell', "Sorry, ask me again in a bit.");
+            handler.emit(':tell', readableTweet);
+        });
+    },
+    'AMAZON.HelpIntent': function () {
+        this.emit(':tell', "Ask me to read Trump's last tweet, and I will.");
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', this.t('Thank you.'));
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', this.t('Bye.'));
+    },
+};
+
+exports.handler = function (event, context) {
+    const alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.appId = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
